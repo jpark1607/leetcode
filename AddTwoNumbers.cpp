22aa6c381d7e4deae5104477d88208520f0b7315
jpark1607 <jpark1607@gmail.com>
2021-10-03 14:21:42 +0900

    AddTwoNumbers Complete

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -1,14 +1,10 @@
 /* Problem 2. Add Two Numbers
+ * https://leetcode.com/submissions/detail/564899156/
  * 1. 문제:
- *
+ *   일의 자리부터 각 자리 수를 표현한 linked list가 두 개 있을 때, 두 수의 합을 구하여라.
  *
  * 2. 아이디어:
- *
- *
- * 3. 상세 코드:
- *   [A]
- *     [A-1]
- *   [B]
+ *   list를 따라가면서 더한다.
  * */
 
 /**
@@ -16,34 +12,52 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *res, *p;
-        int cur;
+        ListNode *res = NULL, *p = NULL;
+        int cur = l1->val + l2->val;
         bool upper = false;
 
-        do {
+        if (cur > 9) {
+            cur -= 10;
+            upper = true;
+        }
+
+        res = new ListNode(cur);
+        p = res;
+
+        l1 = l1->next;
+        l2 = l2->next;
+
+        while (!(l1 == NULL && l2 == NULL && !upper)) {
             cur = 0;
+
             if (upper) {
                 cur++;
                 upper = false;
             }
 
-            if (l1->next != NULL) {
-
+            if (l1 != NULL) {
+                cur += l1->val;
+                l1 = l1->next;
             }
-            if (l2->next != NULL) {
-
+            if (l2 != NULL) {
+                cur += l2->val;
+                l2 = l2->next;
             }
 
+            if (cur > 9) {
+                cur -= 10;
+                upper = true;
+            }
+
+            p->next = new ListNode(cur);
+            p = p->next;
         }
-        while (l1->next != NULL && l2->next != NULL);
 
         return res;
     }
