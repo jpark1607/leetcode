6f5e57b042a1d7ece4e7a7c1927c85a4d6308a20
jpark1607 <jpark1607@gmail.com>
2021-11-14 11:50:02 +0900

    DailyTemperatures Complete

--- DailyTemperatures.cpp
+++ DailyTemperatures.cpp
@@ -1,26 +1,29 @@
 /* Problem 739. Daily Temperatures
- * [http://submissions/here]
+ * https://leetcode.com/submissions/detail/586799058/
  * 1. 문제:
- *
+ *   주어진 vector가 i번째 날의 날씨를 뜻한다고 할 때, i번째 날에서부터 그보다 따뜻해지는 날의 차이를 각각 구하여라.
  *
  * 2. 아이디어:
- *
- *
- * 3. 상세 코드:
- *   [A]
- *     [A-1]
- *   [B]
+ *   stack을 써서 각 날짜보다 따뜻해지는 날에 pop을 한다.
  * */
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int size = temperatures.size();
         vector<int> res(size, 0);
-        int l;
+        stack<int> tmp;
         int i;
 
-        for (i = 1; i < size; i++) {
+        tmp.push(0);
 
+        for (i = 1; i < size; i++) {
+            while (temperatures[i] > temperatures[tmp.top()]) {
+                res[tmp.top()] = i - tmp.top();
+                tmp.pop();
+                if (tmp.empty())
+                    break;
+            }
+            tmp.push(i);
         }
 
         return res;
