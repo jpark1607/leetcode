a1e678a008a71793e98440e6578403c21a3661b1
Jimin Park <jpark1607@gmail.com>
2021-12-02 00:36:41 +0900

    ConstructQuadTree to do

---
+++ ConstructQuadTree.cpp
@@ -0,0 +1,61 @@
+/* Problem 427. Construct Quad Tree
+ * [http://submissions/here]
+ * 1. 문제:
+ *
+ *
+ * 2. 아이디어:
+ *
+ *
+ * 3. 상세 코드:
+ *   [A]
+ *     [A-1]
+ *   [B]
+ * */
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* construct(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+
+    }
+};
